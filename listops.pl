47ce52d55a6d97379b95facc3b74b6e0c011e156
krozet <keawa.rozet@yahoo.com>
2018-03-14 22:14:36 -0700

    KR02 - Created three_adjacent_elements, myappend, mydelete, myinsert, and display_list. Also altered first_element, last_element, and two-adjacent_elements so that they check if certain values are in the array, rather than only displaying the possibilities.

--- listops.pl
+++ listops.pl
@@ -2,19 +2,32 @@ mymember(X, [X|_]).
 mymember(X, [_|T]) :-
   mymember(X, T).
 
-first_element([]).
-first_element([H|_]) :-
-  write(H).
+first_element([X], X).
 
-last_element([]).
-last_element([H|[]]) :-
-  write(H).
-last_element([_|T]) :-
-  last_element(T).
+last_element([X], X).
+last_element([_|T], X) :-
+  last_element(T, X).
 
-two_adjacent_elements([X,Y|[]]) :-
-  write("["), write(X), write(","), write(Y), write("]"), nl.
-two_adjacent_elements([X,Y|T]) :-
-  T \= [],
-  write("["), write(X), write(","), write(Y), write("], "),
-  two_adjacent_elements([Y|T]).
+two_adjacent_elements(X,Y, [X,Y|_]).
+two_adjacent_elements(X,Y, [_|T]) :-
+  two_adjacent_elements(X,Y,T).
+
+three_adjacent_elements(X,Y,Z, [X,Y,Z|_]).
+three_adjacent_elements(X,Y,Z, [_|T]) :-
+  three_adjacent_elements(X,Y,Z,T).
+
+myappend([], H, H).
+myappend([H|T1], H2, [H|T3]) :-
+  myappend(T1, H2, T3).
+
+mydelete(X, [X|T], T).
+mydelete(X, [H|T], [H|T1]) :-
+  mydelete(X, T, T1).
+
+myinsert(X, Y, XY) :-
+  mydelete(X, XY, Y).
+
+display_list([]) :- nl.
+display_list([H|T]) :-
+  write(H), tab(1),
+  display_list(T).
